5bdb38cc268c4e61b1aad3dcbbfe588b5f619ca6
Annie Hsieh <ming.ting@gmail.com>
2013-04-16 10:13:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dtracker::Application.config.secret_token = '8227390652d5cd248224cd4d317af606899cc99091bcba0d1f92699bd4408ec65068ada04ccb038615b40e16caf4adf1d995dd9ac8a0c9d861c83a92f3840701'
